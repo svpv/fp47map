08bbda583ffef46a193c6809ece6bd5190c8584a
Alexey Tourbin <alexey.tourbin@gmail.com>
2018-08-01 05:04:04 +0300

    fpmap.h: redesigned the API to deal with candidates
    Otherwise the probability of birthday collisions is too high,
    specifically for ELF symbols.  Those we have about 8M unique names.

--- fp47map.h
+++ fp47map.h
@@ -18,50 +18,76 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// A "fingerprint map" is a data structure useful for building big string
-// tables (up to a few million strings, up to 4 gigabytes total).  For each
-// string, the caller computes its 64-bit fingerprint, and the data structure
-// provides a 32-bit slot (also called a position).  There is a possibility
-// of a birthday collision (different strings map to the same slot), which
-// the caller must check.  There is also a possibility of insertion failure,
-// in which case fpmap_get returns NULL.  In both cases, the caller should
-// simply rebuild the map from scratch, hashing the strings with a different
-// hash seed.
+// A "fingerprint map" is an auxiliary data structure for building big string
+// tables (many million strings).  The structure maps a fingerprint to the set
+// of candidate positions.  A "fingerprint" is a full 64-bit hash value with
+// good statistical properties.  A "position" is a non-zero 32-bit value which
+// is basically an offset into the string table.  Put simply, the structure
+// maps hash->string; but because 64 bits are not enough to identify strings,
+// the caller should recheck the candidates, typically by calling strcmp(3).
+//
+// There is a fairly small but not completely negligible chance of failure
+// to build the map (insertion can fail, leaving the structure in a somewhat
+// inconsistent state).  In this case, the caller should rebuild the map
+// from scratch, hashing the strings with a different hash seed.
 
 #pragma once
+#include <stddef.h>
 #include <stdint.h>
 
-// The probability of a birthday collision depends on the logsize parameter,
-// the logarithm of the expected size of the map (e.g. 20 for 1M strings).
-// Internally, each fingerprint is digested into a reduced 32-bit hash value
-// and its logsize-bit index, so effectively only 32+logsize bits out of 64
-// bits are used.  Hence the probability of inserting 1 million strings without
-// collisions is about 1e-4, and the probability of successfully inserting 8M
-// strings is about 1e-3 (provided that logsize=20 resp. 23 was specified).
-// The probability of insertion failure is typically much smaller, but the
-// structure has only a limited capability for resizing.  Thus logsize should
-// not be too small, and had better be realistic, otherwise things might not
-// work as expected.
+#ifdef __cplusplus
+extern "C" {
+#else
+#include <stdbool.h>
+#endif
+
+// There are two components that contribute to the probability of failure:
+// 1) cycles in the cuckoo graph (cannot arrange the items into buckets);
+// 2) heavy birthday collisions (more than two items end up with the same hash).
+// Both components depend on the logsize parameter, which is the logarithm of
+// the expected size of the map (e.g. 20 for 1 million strings).  Internally,
+// each fingerprint is digested into a reduced 32-bit hash value and the
+// logsize-bit index at which the value is stored, so effectively only
+// 32+logsize bits out of 64 bits are used.  Although the structure does have
+// a (somewhat limited) capability for resizing, the logsize should not be
+// too small, and had better be a realistic lower bound.
 struct fpmap *fpmap_new(int logsize);
 void fpmap_free(struct fpmap *map);
 
+// Exposes only a part of the structure, just enough to inline the calls.
 struct fpmap {
-    uint32_t *(*get)(void *map, uint64_t);
-    uint32_t *(*has)(void *map, uint64_t);
+    size_t (*find)(void *map, uint64_t fp, uint32_t pos[10]);
+    bool (*insert)(void *map, uint64_t fp, uint32_t pos);
 };
 
-// Get the position on a 64-bit fingerprint.  Returns NULL on insertion failure,
-// which means that a series of evictions failed, and an unrelated slot has been
-// kicked out.  Unless false negatives are permitted, the only option is to
-// rebuild the map from scratch, hashing the strings with a different hash seed.
-uint32_t *fpmap_get(struct fpmap *map, uint64_t fp)
+// Get the candidate positions on a 64-bit fingerprint.
+// Returns the number of positions found (at most 10, typically 0 or 1).
+static inline size_t fpmap_find(struct fpmap *map, uint64_t fp, uint32_t pos[10])
 {
-    return map->get(map, fp);
+    return map->find(map, fp, pos);
 }
 
-// Get the position on a 64-bit fingerprint, but only if the slot was
-// previously created with fpmap_get.  Returns NULL otherwise.
-uint32_t *fpmap_has(struct fpmap *map, uint64_t fp)
+// Compile with -DFPMAP_DEBUG during the development stage.
+#ifdef FPMAP_DEBUG
+#include <assert.h>
+#endif
+
+// Insert a position on a fingerprint.  Dups are not detected (thus fpmap_insert
+// should be called only if fpmap_find failed to locate the matching string).
+// Returns true on success.  Returns false on insertion failure, which means
+// that a series of evictions failed, and an unrelated slot has been kicked out.
+// Unless false negatives are permitted, the only option is to rebuild the map
+// from scratch, hashing the strings with a different hash seed.
+static inline bool fpmap_insert(struct fpmap *map, uint64_t fp, uint32_t pos)
 {
-    return map->has(map, fp);
+    // Recall that positions are non-negative.
+    // Zero is used internally to mark free slots.
+#ifdef FPMAP_DEBUG
+    assert(pos > 0);
+#endif
+    return map->insert(map, fp, pos);
 }
+
+#ifdef __cplusplus
+}
+#endif
