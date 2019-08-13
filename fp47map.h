470c7062e1a9f03b6c96fd62b5cc52f36486a7f8
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-08-13 14:02:31 +0300

    fpmap.h: another redesign!
    I have a few half-working versions of this data structure, now trying
    to put them together.  Designing a good API turns out to be much harder
    than merely getting things half-working. :-)

--- fp47map.h
+++ fp47map.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, 2018 Alexey Tourbin
+// Copyright (c) 2017, 2018, 2019 Alexey Tourbin
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,75 +18,152 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// A "fingerprint map" is an auxiliary data structure for building big string
-// tables (many million strings).  The structure maps a fingerprint to the set
-// of candidate positions.  A "fingerprint" is a full 64-bit hash value with
-// good statistical properties.  A "position" is a non-zero 32-bit value which
-// is basically an offset into the string table.  Put simply, the structure
-// maps hash->string; but because 64 bits are not enough to identify strings,
-// the caller should recheck the candidates, typically by calling strcmp(3).
+// A "fingerprint map" a low-level bucket manager that serves as the basis for
+// hash tables.  It manages "bucket entries" of the two flavors:
 //
-// There is a fairly small but not completely negligible chance of failure
-// to build the map (insertion can fail, leaving the structure in a somewhat
-// inconsistent state).  In this case, the caller should rebuild the map
-// from scratch, hashing the strings with a different hash seed.
+//	#define FPMAP_FPTAG_BITS 16
+//	struct fpmap_bent {
+//	    ...                   // user data
+//	    const uint16_t fptag; // fingerprint tag
+//	};
+//
+//	#define FPMAP_FPTAG_BITS 32
+//	struct fpmap_bent {
+//	    ...                   // user data
+//	    const uint32_t fptag; // fingerprint tag
+//	};
+//
+// To look up the entries, the caller supplies fingerprints.  A "fingerprint" is
+// a 64-bit hash value with good statistical properties.  It further gets digested
+// into two pieces: the index to locate the bucket, and the "fingerptint tag"
+// to recheck the entries in the bucket.  (The actual scheme is a bit more
+// complicated: we check two buckets, and the fingerprint is also responsible
+// for locating the second bucket.  This scheme is known as the cuckoo filter.)
+// Thus the data structure is conceptually similar to multimap<hash,data>:
+// it leaves it up to the caller to compare the keys for exact equality.
+//
+// Along with struct fpmap_bent and FPMAP_FPTAG_BITS, the caller must define
+// FPMAP_BENT_SIZE, which is a decimal literal equal to sizeof(struct fpmap_bent).
+// The literals are used to give distinct names to ABI functions, so that it is
+// possible to use a few different maps in the same program.  (Inline functions
+// are not renamed though, so currently it isn't possible to use two different
+// maps in a single source file.)
 
-#pragma once
+#ifndef __cplusplus
 #include <stddef.h>
 #include <stdint.h>
-
-#ifdef __cplusplus
-extern "C" {
-#else
 #include <stdbool.h>
+#include <assert.h>
+#else
+#include <cstddef>
+#include <cstdint>
+extern "C" {
 #endif
 
-// There are two components that contribute to the probability of failure:
-// 1) cycles in the cuckoo graph (cannot arrange the items into buckets);
-// 2) heavy birthday collisions (more than two items end up with the same hash).
-// Both components depend on the logsize parameter, which is the logarithm of
-// the expected size of the map (e.g. 20 for 1 million strings).  Internally,
-// each fingerprint is digested into a reduced 32-bit hash value and the
-// logsize-bit index at which the value is stored, so effectively only
-// 32+logsize bits out of 64 bits are used.  Although the structure does have
-// a (somewhat limited) capability for resizing, the logsize should not be
-// too small, and had better be a realistic lower bound.
+// Beta version, static linking only.
+#ifdef __GNUC__
+#pragma GCC visibility push(hidden)
+#endif
+
+#define FPMAP_NAME3_(h, v, name) fp##h##map##v##_##name
+#define FPMAP_NAME3(h, v, name) FPMAP_NAME3_(h, v, name)
+
+#if FPMAP_FPTAG_BITS == 16
+#define FPMAP_NAME(name) FPMAP_NAME3(31, FPMAP_BENT_SIZE, name)
+#elif FPMAP_FPTAG_BITS == 32
+#define FPMAP_NAME(name) FPMAP_NAME3(47, FPMAP_BENT_SIZE, name)
+#else
+#error "invalid FPMAP_FPTAG_BITS"
+#endif
+
+static_assert(sizeof(struct fpmap_bent) == FPMAP_BENT_SIZE, "");
+static_assert(offsetof(struct fpmap_bent, fptag) == FPMAP_BENT_SIZE - FPMAP_FPTAG_BITS / 8,
+	"fptag the last member of struct fpmap_bent");
+
+#define fpmap_new FPMAP_NAME(new)
+#define fpmap_free FPMAP_NAME(free)
+
+// Create a map.  The logsize parameter specifies the expected number of
+// entries in the map (e.g. logsize = 10 for 1024).  There is a fairly small
+// but not completely negligible chance of failure to build the map (this is
+// true for any hash-based implementation which limits its worst-case
+// behavior.)  The failure rate depends on the initial logsize value:
+// the bigger the table, the smaller the chance that it breaks.  Therefore,
+// logsize should not be too small, and had better be a realistic lower bound.
 struct fpmap *fpmap_new(int logsize);
 void fpmap_free(struct fpmap *map);
 
-// Exposes only a part of the structure, just enough to inline the calls.
+// i386 convention: on Windows, stick to fastcall, for compatibility with msvc.
+#if (defined(_WIN32) || defined(__CYGWIN__)) && \
+    (defined(_M_IX86) || defined(__i386__))
+#define FPMAP_MSFASTCALL 1
+#if defined(__GNUC__)
+#define FPMAP_FASTCALL __attribute__((fastcall))
+#else
+#define FPMAP_FASTCALL __fastcall
+#endif
+#else // otherwise, use regparm(3).
+#define FPMAP_MSFASTCALL 0
+#if defined(__i386__)
+#define FPMAP_FASTCALL __attribute__((regparm(3)))
+#else
+#define FPMAP_FASTCALL
+#endif
+#endif
+
+// fastcall has trouble passing uint64_t in registers.
+#if FPMAP_MSFASTCALL
+#define FPMAP_pFP64 uint32_t lo, uint32_t hi
+#define FPMAP_aFP64(fp) fp, fp >> 32
+#else
+#define FPMAP_pFP64 uint64_t fp
+#define FPMAP_aFP64(fp) fp
+#endif
+
+// Expose the structure, to inline vfunc calls.
 struct fpmap {
-    size_t (*find)(void *map, uint64_t fp, uint32_t pos[10]);
-    bool (*insert)(void *map, uint64_t fp, uint32_t pos);
+    // To reduce the failure rate, one or two entries can be stashed.
+    // This guy goes first and gets the best alignment (the same as buckets).
+    struct fpmap_stash {
+	struct fpmap_bent bent[2];
+	uint32_t lo[2];
+    } stash;
+    // Virtual functions, depend on the bucket size, switched on resize.
+    // Pass fp arg first, eax:edx may hold hash() return value.
+    size_t (FPMAP_FASTCALL *find)(FPMAP_pFP64, const struct fpmap *set, struct fpmap_bent *match[10]);
+    struct fpmap_bent *(FPMAP_FASTCALL *insert)(FPMAP_pFP64, struct fpmap *map);
+    // The buckets (malloc'd); each bucket has bsize entries.
+    // Two-dimensional structure is emulated with pointer arithmetic.
+    struct fpmap_bent *bb;
+    // The total number of entries added to buckets,
+    // not including the stashed entries.
+    size_t cnt;
+    // The number of entries in each bucket: 2, 3, or 4.
+    uint8_t bsize;
+    // The number of stashed entries: 0, 1, or 2.
+    uint8_t nstash;
+    // The number of buckets, initial and current, the logarithm: 4..32.
+    uint8_t logsize0, logsize1;
+    // The corresponding masks, help indexing into the buckets.
+    uint32_t mask0, mask1;
 };
 
-// Get the candidate positions on a 64-bit fingerprint.
-// Returns the number of positions found (at most 10, typically 0 or 1).
-static inline size_t fpmap_find(struct fpmap *map, uint64_t fp, uint32_t pos[10])
+// Obtain the set of entries matching the fingerprint.
+// Returns the number of matches found (at most 10, typically 0 or 1).
+static inline size_t fpmap_find(struct fpmap *map, uint64_t fp, struct fpmap_bent *match[10])
 {
-    return map->find(map, fp, pos);
+    return map->find(FPMAP_aFP64(fp), map, match);
 }
 
-// Compile with -DFPMAP_DEBUG during the development stage.
-#ifdef FPMAP_DEBUG
-#include <assert.h>
-#endif
-
-// Insert a position on a fingerprint.  Dups are not detected (thus fpmap_insert
-// should be called only if fpmap_find failed to locate the matching string).
-// Returns true on success.  Returns false on insertion failure, which means
-// that a series of evictions failed, and an unrelated slot has been kicked out.
-// Unless false negatives are permitted, the only option is to rebuild the map
-// from scratch, hashing the strings with a different hash seed.
-static inline bool fpmap_insert(struct fpmap *map, uint64_t fp, uint32_t pos)
+// Request a new entry associated with the fingerprint.
+static inline struct fpmap_bent *fpmap_insert(struct fpmap *map, uint64_t fp)
 {
-    // Recall that positions are non-negative.
-    // Zero is used internally to mark free slots.
-#ifdef FPMAP_DEBUG
-    assert(pos > 0);
-#endif
-    return map->insert(map, fp, pos);
+    return map->insert(FPMAP_aFP64(fp), map);
 }
+
+#ifdef __GNUC__
+#pragma GCC visibility pop
+#endif
 
 #ifdef __cplusplus
 }
