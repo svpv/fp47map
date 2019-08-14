b477e461561fd7d5154ae7319c5ada154e835e4d
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-08-14 10:33:54 +0300

    fpmap.c: overhauled
    The resizing code is still lacking, but at least I finally got to commit
    a known good state.

--- fp47map.c
+++ fp47map.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, 2018 Alexey Tourbin
+// Copyright (c) 2017, 2018, 2019 Alexey Tourbin
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,40 +18,21 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "fpmap.h"
+#include <stdint.h>
 
-// The bucket entry, maps fp32->pos.  Note however that the match is determined
-// by the right fingerprint at the right bucket.  The same 32-bit fingerprint
-// can have different meaning depending on its location in the table.
-struct bent {
-    uint32_t fp32;
-    uint32_t pos;
-} __attribute__((aligned(8)));
-
-// The real fpmap structure (upconverted from "void *map").
-struct map {
-    // Virtual functions.
-    size_t (*find)(void *map, uint64_t fp, uint32_t pos[10]);
-    bool (*insert)(void *map, uint64_t fp, uint32_t pos);
-    // The number of slots in each bucket: 2, 3, or 4.
-    uint8_t bsize;
-    // The number of fingerprints stashed: 0, 1, or 2.
-    uint8_t nstash;
-    // The number of buckets, initial and current, the logarithm: 3..30.
-    uint8_t logsize0, logsize1;
-    // The corresponding masks, help indexing into the buckets.
-    uint32_t mask0, mask1;
-    // The total number of entries in the buckets, not including the stash.
-    uint32_t cnt;
-    // The buckets (malloc'd); each bucket has bsize slots.
-    // Two-dimensional structure is emulated with pointer arithmetic.
-    struct bent *bb;
-    // To reduce the failure rate, one or two entries can be stashed.
-    struct stash {
-	uint64_t key[2];
-	uint32_t pos[2];
-    } stash;
+// The implementation does not depend on the caller's struct fpmap_bent
+// exact definition, only on its size.
+struct fpmap_bent {
+#if FPMAP_FPTAG_BITS == 16
+    unsigned char data[FPMAP_BENT_SIZE-2];
+    uint16_t fptag;
+#else
+    unsigned char data[FPMAP_BENT_SIZE-4];
+    uint32_t fptag;
+#endif
 };
+
+#include "fpmap.h"
 
 #define unlikely(cond) __builtin_expect(cond, 0)
 
@@ -88,273 +69,167 @@ struct map {
     } while (0)
 #define Sort2 Sort2swap
 
-#ifdef __SSE2__
-#include <emmintrin.h>
-#endif
+#define mod16(fp) (1 + (uint32_t)(fp) % UINT16_MAX)
+#define mod32(fp) (1 + (fp) % UINT32_MAX)
+#define Golden32 UINT32_C(2654435761)
 
-#include <limits.h>
-
-// On 64-bit systems, assume malloc'd chunks are aligned to 16 bytes.
-// This should help to elicit aligned SSE2 instructions.
-#if SIZE_MAX > UINT32_MAX
-#define A16(p) __builtin_assume_aligned(p, 16)
+#if FPMAP_FPTAG_BITS == 16
+#define dI1T					\
+    uint32_t i1 = fp >> 32;			\
+    uint32_t fptag = mod16(fp);			\
+    uint32_t xorme = fptag * Golden32
 #else
-// Already assuming that "struct bent" is 8-byte aligned.
-#define A16(p) __builtin_assume_aligned(p, 8)
-// Disable SSE2 aligned intrinsics.
-#define _mm_load_si128 _mm_loadu_si128
+#define dI1T					\
+    uint32_t i1 = fp;				\
+    uint32_t fptag = mod32(fp);			\
+    uint32_t xorme = fptag
 #endif
+
+// Declare the variables which map the fingerprint to indexes and buckets.
+// Note that the two buckets are completely symmetrical with regard to xor,
+// i.e. the information about "the first and true" index is not preserved.
+// This looses about 1 bit out of 32+logsize bits of hashing material.
+#define dFP2IB					\
+    dI1T;					\
+    uint32_t i2 = i1 ^ xorme;			\
+    i1 &= map->mask0;				\
+    i2 &= map->mask0;				\
+    /* Indexes need extra high bits? */		\
+    if (resized) {				\
+	Sort2(i1, i2);				\
+	i1 |= fptag << map->logsize0;		\
+	i2 = i1 ^ xorme;			\
+	i1 &= map->mask1;			\
+	i2 &= map->mask1;			\
+    }						\
+    struct fpmap_bent *b1, *b2;			\
+    b1 = map->bb + i1 * bsize;			\
+    b2 = map->bb + i2 * bsize
 
 // Template for map->find virtual functions.
-static inline size_t t_find(struct map *map, uint64_t fp, uint32_t pos[10],
-	uint8_t bsize, bool resized, uint8_t nstash)
+static inline size_t t_find(const struct fpmap *map, uint64_t fp, struct fpmap_bent *match[10],
+	int bsize, bool resized, int nstash)
 {
-    // So a fingerprint is digested into a smaller 32-bit fingerprint (by
-    // simply taking lower 32 bits) and two buckets at which it can reside.
-    // Note that the buckets are completely symmetrical with regard to "xor fp32",
-    // i.e. the information about "the first and true" index is not preserved.
-    // This looses about 1 bit out of 32+logsize bits of hashing material.
-    uint32_t fp32 = fp;
-    size_t i1 = fp >> 32;
-    size_t i2 = i1 ^ fp;
-    i1 &= map->mask0;
-    i2 &= map->mask0;
-
-    // Need to sort i1 < i2?
-    if (resized || nstash)
-	Sort2(i1, i2);
-
-    // Key with canonical index, to look up in the stash.
-    uint64_t stkey;
-    if (nstash)
-	stkey = fp32 | (uint64_t) i1 << 32;
-
-    // Indexes need extra high bits?
-    if (resized) {
-	i1 |= fp32 << map->logsize0;
-	i2 = i1 ^ fp;
-	i1 &= map->mask1;
-	i2 &= map->mask1;
-    }
-
-    // Indexes are ready, at last the buckets.
-    struct bent *b1 = map->bb + i1 * bsize;
-    struct bent *b2 = map->bb + i2 * bsize;
-    if (nstash)
-	__builtin_prefetch(b1);
-    __builtin_prefetch(b2);
-
-    // The result.
+    dFP2IB;
     size_t n = 0;
 
     // Branches are predictable, no need for cmov.
-#define AddMatch(Key, Fp, Pos)		\
+#if 1
+#define AddMatch(tag1, tag2, ent)	\
     do {				\
-	if (unlikely(Key == Fp) && Pos)	\
-	    pos[n++] = Pos;		\
+	if (unlikely(tag1 == tag2))	\
+	    match[n++] = ent;		\
     } while (0)
-
-    // Check the stash.
-    struct stash *st = &map->stash;
-    if (nstash > 0) AddMatch(stkey, st->key[0], st->pos[0]);
-    if (nstash > 1) AddMatch(stkey, st->key[1], st->pos[1]);
-
-    // SSE2 optimization frenzy.
-#define CheckBucketMask(b, m)					\
-    do {							\
-	if (unlikely(m & 0x0f0f)) {				\
-	    if ((m & 0x00ff) == 0x000f)				\
-		pos[n++] = b[0].pos;				\
-	    if ((m & 0xff00) == 0x0f00)				\
-		pos[n++] = b[1].pos;				\
-	}							\
+#else
+#define AddMatch(tag1, tag2, ent)	\
+    do {				\
+	match[n] = ent;			\
+	n += (tag1 == tag2);		\
     } while (0)
-#define CheckBucketXmm(b, xmm)					\
-    do {							\
-	xmm = _mm_cmpeq_epi32(xmm, fp32x2);			\
-	int m = _mm_movemask_epi8(xmm);				\
-	CheckBucketMask(b, m);					\
-    } while (0)
-#define CheckBucket2(b)						\
-    do {							\
-	__m128i xmm = _mm_load_si128((void *) b);		\
-	CheckBucketXmm(b, xmm);					\
-    } while (0)
-#define CheckBucket3(b)						\
-    do {							\
-	__m128i xmm = _mm_loadu_si128((void *) b);		\
-	CheckBucketXmm(b, xmm);					\
-	AddMatch(fp32, b[2].fp32, b[2].pos);			\
-    } while (0)
-#define CheckBucket4(b)						\
-    do {							\
-	__m128i xmm1 = _mm_load_si128((__m128i *) b + 0);	\
-	__m128i xmm2 = _mm_load_si128((__m128i *) b + 1);	\
-	xmm1 = _mm_cmpeq_epi32(xmm1, fp32x2);			\
-	xmm2 = _mm_cmpeq_epi32(xmm2, fp32x2);			\
-	int m1 = _mm_movemask_epi8(xmm1);			\
-	int m2 = _mm_movemask_epi8(xmm2);			\
-	CheckBucketMask(b, m1);					\
-	CheckBucketMask(b, m2);					\
-    } while (0)
-
-#ifdef __SSE2__
-    __m128i fp32x2 = _mm_set_epi32(0, fp32, 0, fp32);
-    if (bsize == 2) {
-	CheckBucket2(b1);
-	CheckBucket2(b2);
-    }
-    if (bsize == 3) {
-	CheckBucket3(b1);
-	CheckBucket3(b2);
-    }
-    if (bsize == 4) {
-	CheckBucket4(b1);
-	CheckBucket4(b2);
-    }
-    return n;
 #endif
 
-    // Check the first bucket.
-    AddMatch(fp32, b1[0].fp32, b1[0].pos);
-    AddMatch(fp32, b1[1].fp32, b1[1].pos);
-    if (bsize > 2) AddMatch(fp32, b1[2].fp32, b1[2].pos);
-    if (bsize > 3) AddMatch(fp32, b1[3].fp32, b1[3].pos);
+    if (bsize > 0) AddMatch(fptag, b1[0].fptag, &b1[0]);
+    if (bsize > 0) AddMatch(fptag, b2[0].fptag, &b2[0]);
 
-    // The second bucket has been prefetched by now.
-    AddMatch(fp32, b2[0].fp32, b2[0].pos);
-    AddMatch(fp32, b2[1].fp32, b2[1].pos);
-    if (bsize > 2) AddMatch(fp32, b2[2].fp32, b2[2].pos);
-    if (bsize > 3) AddMatch(fp32, b2[3].fp32, b2[3].pos);
+    struct fpmap_bent *st = (void *) map->stash.bent; // const cast
+    if (nstash > 0) AddMatch(fptag, st[0].fptag, &st[0]);
+    if (nstash > 1) AddMatch(fptag, st[1].fptag, &st[1]);
+
+    if (bsize > 1) AddMatch(fptag, b1[1].fptag, &b1[1]);
+    if (bsize > 1) AddMatch(fptag, b2[1].fptag, &b2[1]);
+    if (bsize > 2) AddMatch(fptag, b1[2].fptag, &b1[2]);
+    if (bsize > 2) AddMatch(fptag, b2[2].fptag, &b2[2]);
+    if (bsize > 3) AddMatch(fptag, b1[3].fptag, &b1[3]);
+    if (bsize > 3) AddMatch(fptag, b2[3].fptag, &b2[3]);
 
     return n;
 }
 
 // Virtual functions for map->find.
-static size_t fpmap_find2re0st0(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 2, 0, 0); }
-static size_t fpmap_find2re0st1(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 2, 0, 1); }
-static size_t fpmap_find2re0st2(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 2, 0, 2); }
-static size_t fpmap_find3re0st0(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 3, 0, 0); }
-static size_t fpmap_find3re0st1(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 3, 0, 1); }
-static size_t fpmap_find3re0st2(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 3, 0, 2); }
-static size_t fpmap_find3re1st0(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 3, 1, 0); }
-static size_t fpmap_find3re1st1(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 3, 1, 1); }
-static size_t fpmap_find3re1st2(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 3, 1, 2); }
-static size_t fpmap_find4re0st0(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 4, 0, 0); }
-static size_t fpmap_find4re0st1(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 4, 0, 1); }
-static size_t fpmap_find4re0st2(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 4, 0, 2); }
-static size_t fpmap_find4re1st0(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 4, 1, 0); }
-static size_t fpmap_find4re1st1(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 4, 1, 1); }
-static size_t fpmap_find4re1st2(void *map, uint64_t fp, uint32_t pos[10]) { return t_find(map, fp, pos, 4, 1, 2); }
+#define MakeVFuncs(BS, RE, ST) \
+    static FPMAP_FASTCALL size_t FPMAP_NAME(find##BS##re##RE##st##ST) \
+	(FPMAP_pFP64, const struct fpmap *map, struct fpmap_bent *match[10]) \
+    { return t_find(map, fp, match, BS, RE, ST); }
+#define MakeAllVFuncs	\
+    MakeVFuncs(2, 0, 0) MakeVFuncs(2, 0, 1) MakeVFuncs(2, 0, 2) \
+    MakeVFuncs(3, 0, 0) MakeVFuncs(3, 0, 1) MakeVFuncs(3, 0, 2) \
+    MakeVFuncs(4, 0, 0) MakeVFuncs(4, 0, 1) MakeVFuncs(4, 0, 2) \
+    MakeVFuncs(3, 1, 0) MakeVFuncs(3, 1, 1) MakeVFuncs(3, 1, 2) \
+    MakeVFuncs(4, 1, 0) MakeVFuncs(4, 1, 1) MakeVFuncs(4, 1, 2)
+MakeAllVFuncs
 
-static inline bool justAdd(struct bent *b1, struct bent *b2,
-	struct bent be, uint8_t bsize)
+static inline struct fpmap_bent *insert(struct fpmap_bent *b1, struct fpmap_bent *b2,
+	int bsize)
 {
-    if (b1[0].pos == 0) return b1[0] = be, true;
-    if (b2[0].pos == 0) return b2[0] = be, true;
-    if (b1[1].pos == 0) return b1[1] = be, true;
-    if (b2[1].pos == 0) return b2[1] = be, true;
-    if (bsize > 2 && b1[2].pos == 0) return b1[2] = be, true;
-    if (bsize > 2 && b2[2].pos == 0) return b2[2] = be, true;
-    if (bsize > 3 && b1[3].pos == 0) return b1[3] = be, true;
-    if (bsize > 3 && b2[3].pos == 0) return b2[3] = be, true;
-    return false;
+    if (bsize > 0 && b1[0].fptag == 0) return &b1[0];
+    if (bsize > 0 && b2[0].fptag == 0) return &b2[0];
+    if (bsize > 1 && b1[1].fptag == 0) return &b1[1];
+    if (bsize > 1 && b2[1].fptag == 0) return &b2[1];
+    if (bsize > 2 && b1[2].fptag == 0) return &b1[2];
+    if (bsize > 2 && b2[2].fptag == 0) return &b2[2];
+    if (bsize > 3 && b1[3].fptag == 0) return &b1[3];
+    if (bsize > 3 && b2[3].fptag == 0) return &b2[3];
+    return NULL;
 }
 
-#include <string.h>
-
-// Unlike memcpy, memmove(dst, src, 16) may end up not inlined.
-// Hence the specialized code to shift two bucket entries down.
-static inline void MoveDown2(struct bent *b)
+static inline bool kick(struct fpmap_bent be, struct fpmap_bent *bb, struct fpmap_bent *b,
+	size_t i, struct fpmap_bent *obe, uint32_t *oi, int logsize, uint32_t mask, int bsize)
 {
-#if SIZE_MAX > UINT32_MAX || defined(__x86_64__)
-    uint64_t buf[2];
-    memcpy(buf, b + 1, 16);
-    memcpy(b, buf, 16);
-#else
-    b[0] = b[1];
-    b[1] = b[2];
-#endif
-}
-
-static inline bool kickAdd(struct map *map, struct bent *b, struct bent be, size_t ix,
-	struct bent *obe, size_t *oix, uint8_t bsize, bool resized)
-{
-    int k = 2 * map->logsize1;
+    int maxkick = 2 * logsize;
     do {
 	// Put at the top, kick out from the bottom.
 	// Using *obe as a temporary register.
 	*obe = b[0];
-	if (bsize == 2)
-	    b[0] = b[1];
-	else if (bsize == 3)
-	    MoveDown2(b);
-	else {
-	    MoveDown2(A16(b));
-	    b[2] = b[3];
-	}
-	b[bsize-1] = be, be = *obe;
+	if (bsize > 1) b[0] = b[1];
+	if (bsize > 2) b[1] = b[2];
+	if (bsize > 3) b[2] = b[3];
+	b[bsize-1] = be;
 	// Ponder over the entry that's been kicked out.
 	// Find out the alternative bucket.
-	ix ^= be.fp32;
-	ix &= resized ? map->mask1 : map->mask0;
-	b = map->bb + bsize * ix;
+	uint32_t xorme = obe->fptag;
+#if FPMAP_FPTAG_BITS == 16
+	xorme *= Golden32;
+#endif
+	i ^= xorme;
+	i &= mask;
+	b = bb + bsize * i;
 	// Insert to the alternative bucket.
-	if (b[0].pos == 0) return b[0] = be, true;
-	if (b[1].pos == 0) return b[1] = be, true;
-	if (bsize > 2 && b[2].pos == 0) return b[2] = be, true;
-	if (bsize > 3 && b[3].pos == 0) return b[3] = be, true;
-    } while (k-- > 0);
+	if (bsize > 0 && b[0].fptag == 0) return b[0] = *obe, true;
+	if (bsize > 1 && b[1].fptag == 0) return b[1] = *obe, true;
+	if (bsize > 2 && b[2].fptag == 0) return b[2] = *obe, true;
+	if (bsize > 3 && b[3].fptag == 0) return b[3] = *obe, true;
+	be = *obe;
+    } while (--maxkick >= 0);
     // Ran out of tries? obe already set.
-    *oix = ix;
+    *oi = i;
     return false;
 }
 
 // Template for map->insert virtual functions.
-static inline bool t_insert(struct map *map, uint64_t fp, uint32_t pos,
-	uint8_t bsize, bool resized, uint8_t nstash)
+static inline struct fpmap_bent *t_insert(struct fpmap *map, uint64_t fp,
+	int bsize, bool resized, int nstash)
 {
-    uint32_t fp32 = fp;
-    size_t i1 = fp >> 32;
-    size_t i2 = i1 ^ fp;
-    i1 &= map->mask0;
-    i2 &= map->mask0;
-
-    if (resized) {
-	Sort2(i1, i2);
-	i1 |= fp32 << map->logsize0;
-	i2 = i1 ^ fp;
-	i1 &= map->mask1;
-	i2 &= map->mask1;
+    dFP2IB;
+    struct fpmap_bent *be = insert(b1, b2, bsize);
+    if (be) {
+	be->fptag = fptag;
+	return map->cnt++, be;
     }
-
-    struct bent be = { fp32, pos };
-    struct bent *b1 = map->bb + i1 * bsize;
-    struct bent *b2 = map->bb + i2 * bsize;
-
-    if (justAdd(b1, b2, be, bsize))
-	return map->cnt++, true;
-    if (kickAdd(map, b1, be, i1, &be, &i1, bsize, resized))
-	return map->cnt++, true;
-    return false;
+    struct fpmap_bent kbe = { .fptag = fptag };
+    int logsize = resized ? map->logsize1 : map->logsize0;
+    uint32_t mask = resized ? map->mask1 : map->mask0;
+    if (kick(kbe, map->bb, b1, i1, &kbe, &i1, logsize, mask, bsize))
+	return map->cnt++, &b1[bsize-1];
+    return NULL;
 }
 
 // Virtual functions for map->insert.
-static bool fpmap_insert2re0st0(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 2, 0, 0); }
-static bool fpmap_insert2re0st1(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 2, 0, 1); }
-static bool fpmap_insert2re0st2(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 2, 0, 2); }
-static bool fpmap_insert3re0st0(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 3, 0, 0); }
-static bool fpmap_insert3re0st1(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 3, 0, 1); }
-static bool fpmap_insert3re0st2(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 3, 0, 2); }
-static bool fpmap_insert3re1st0(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 3, 1, 0); }
-static bool fpmap_insert3re1st1(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 3, 1, 1); }
-static bool fpmap_insert3re1st2(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 3, 1, 2); }
-static bool fpmap_insert4re0st0(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 4, 0, 0); }
-static bool fpmap_insert4re0st1(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 4, 0, 1); }
-static bool fpmap_insert4re0st2(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 4, 0, 2); }
-static bool fpmap_insert4re1st0(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 4, 1, 0); }
-static bool fpmap_insert4re1st1(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 4, 1, 1); }
-static bool fpmap_insert4re1st2(void *map, uint64_t fp, uint32_t pos) { return t_insert(map, fp, pos, 4, 1, 2); }
+#undef MakeVFuncs
+#define MakeVFuncs(BS, RE, ST) \
+    static FPMAP_FASTCALL struct fpmap_bent *FPMAP_NAME(insert##BS##re##RE##st##ST) \
+	(FPMAP_pFP64, struct fpmap *map) \
+    { return t_insert(map, fp, BS, RE, ST); }
+MakeAllVFuncs
 
 #include <assert.h>
 #include <stdlib.h>
@@ -363,23 +238,23 @@ static bool fpmap_insert4re1st2(void *map, uint64_t fp, uint32_t pos) { return t
 struct fpmap *fpmap_new(int logsize)
 {
     assert(logsize >= 0);
-    if (logsize < 3)
-	logsize = 3;
-    if (logsize > 30)
+    if (logsize < 4)
+	logsize = 4;
+    if (logsize > 32)
 	return errno = E2BIG, NULL;
 
     // Starting with two slots per bucket.
     size_t nb = (size_t) 1 << logsize;
-    struct bent *bb = calloc(nb, 2 * sizeof(struct bent));
+    struct fpmap_bent *bb = calloc(nb, 2 * sizeof(struct fpmap_bent));
     if (!bb)
 	return NULL;
 
-    struct map *map = malloc(sizeof *map);
+    struct fpmap *map = malloc(sizeof *map);
     if (!map)
 	return free(bb), NULL;
 
-    map->find = fpmap_find2re0st0;
-    map->insert = fpmap_insert2re0st0;
+    map->find = FPMAP_NAME(find2re0st0);
+    map->insert = FPMAP_NAME(insert2re0st0);
     map->bsize = 2;
     map->nstash = 0;
     map->logsize0 = map->logsize1 = logsize;
@@ -391,19 +266,19 @@ struct fpmap *fpmap_new(int logsize)
 
 void fpmap_free(struct fpmap *arg)
 {
-    struct map *map = (void *) arg;
+    struct fpmap *map = (void *) arg;
     if (!map)
 	return;
 #ifdef FPMAP_DEBUG
     // The number of entries must match the occupied slots.
     size_t cnt = 0;
-    struct bent *bb = map->bb;
+    struct fpmap_bent *bb = map->bb;
     size_t n = map->bsize * (map->mask1 + 1);
     for (size_t i = 0; i < n; i += 4)
-	cnt += (bb[i+0].pos > 0)
-	    +  (bb[i+1].pos > 0)
-	    +  (bb[i+2].pos > 0)
-	    +  (bb[i+3].pos > 0);
+	cnt += (bb[i+0].fptag > 0)
+	    +  (bb[i+1].fptag > 0)
+	    +  (bb[i+2].fptag > 0)
+	    +  (bb[i+3].fptag > 0);
     assert(cnt == map->cnt);
 #endif
     free(map->bb);
