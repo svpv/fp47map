ccb6aeebe810b5146295431415287d152782ec1d
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-09-03 17:09:55 +0300

    fp47map.h: redesign for slim buckets!
    I've made some more experiments.  Storing 8-byte values directly in
    the buckets turns out to be slower than storing 4-byte array indexes.
    While slower, it takes a bit less memory, but this won't be the case
    with bigger values that will waste memory because of free slots and
    the limited fill factor.
    
    This is a bit surprising, and I don't have full explanation, but on
    rpmelfsym-like loads the difference is well measurable and not
    negligible.
    
    So I've made up my mind: instead of generalized struct fpmap_bent,
    I'll press on with uint32_t pos.
    
    The git history has been rewritten, files renamed fpmap->fp47map,
    with otherwise no changes.

--- fp47map.h
+++ fp47map.h
@@ -19,41 +19,30 @@
 // SOFTWARE.
 
 // A "fingerprint map" a low-level bucket manager that serves as the basis for
-// hash tables.  It manages "bucket entries" of the two flavors:
+// hash tables.  Internally it manages "bucket entries" of this kind:
 //
-//	#define FPMAP_FPTAG_BITS 16
-//	struct fpmap_bent {
-//	    ...                   // user data
-//	    const uint16_t fptag; // fingerprint tag
+//	struct bent {
+//	    uint32_t pos;   // position
+//	    uint32_t fptag; // fingerprint tag
 //	};
 //
-//	#define FPMAP_FPTAG_BITS 32
-//	struct fpmap_bent {
-//	    ...                   // user data
-//	    const uint32_t fptag; // fingerprint tag
-//	};
-//
-// To look up the entries, the caller supplies fingerprints.  A "fingerprint" is
-// a 64-bit hash value with good statistical properties.  It further gets digested
-// into two pieces: the index to locate the bucket, and the "fingerptint tag"
-// to recheck the entries in the bucket.  (The actual scheme is a bit more
-// complicated: we check two buckets, and the fingerprint is also responsible
-// for locating the second bucket.  This scheme is known as the cuckoo filter.)
-// Thus the data structure is conceptually similar to multimap<hash,data>:
+// These entries associate fingerprints with positions.  A "position" is
+// 32-bit user data (typically an array index), and can be of any value
+// (including 0 and UINT32_MAX).  To insert entries / look up positions,
+// the caller supplies fingerprints.  A "fingerprint" is a 64-bit hash value
+// with good statistical properties.  It further gets split into two pieces:
+// the index to locate the bucket, and the "fingerptint tag" to recheck the
+// entries in the bucket.  The tag is calculated in such a way that it is
+// non-zero, while zeros mark empty slots.  (The actual scheme is a bit more
+// complicated: we check two buckets, and the tag is also responsible for
+// locating the second bucket.  This scheme is known as the cuckoo filter.)
+// Thus the data structure is conceptually similar to multimap<hash,pos>:
 // it leaves it up to the caller to compare the keys for exact equality.
-//
-// Along with struct fpmap_bent and FPMAP_FPTAG_BITS, the caller must define
-// FPMAP_BENT_SIZE, which is a decimal literal equal to sizeof(struct fpmap_bent).
-// The literals are used to give distinct names to ABI functions, so that it is
-// possible to use a few different maps in the same program.  (Inline functions
-// are not renamed though, so currently it isn't possible to use two different
-// maps in a single source file.)
 
+#pragma once
 #ifndef __cplusplus
 #include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include <assert.h>
 #else
 #include <cstddef>
 #include <cstdint>
@@ -65,115 +54,66 @@ extern "C" {
 #pragma GCC visibility push(hidden)
 #endif
 
-#define FPMAP_NAME3_(h, v, name) fp##h##map##v##_##name
-#define FPMAP_NAME3(h, v, name) FPMAP_NAME3_(h, v, name)
-
-#if FPMAP_FPTAG_BITS == 16
-#define FPMAP_NAME(name) FPMAP_NAME3(31, FPMAP_BENT_SIZE, name)
-#elif FPMAP_FPTAG_BITS == 32
-#define FPMAP_NAME(name) FPMAP_NAME3(47, FPMAP_BENT_SIZE, name)
-#else
-#error "invalid FPMAP_FPTAG_BITS"
-#endif
-
-static_assert(sizeof(struct fpmap_bent) == FPMAP_BENT_SIZE, "");
-static_assert(offsetof(struct fpmap_bent, fptag) == FPMAP_BENT_SIZE - FPMAP_FPTAG_BITS / 8,
-	"fptag the last member of struct fpmap_bent");
-
-#define fpmap_new FPMAP_NAME(new)
-#define fpmap_free FPMAP_NAME(free)
-#define fpmap_next FPMAP_NAME(next)
-
 // Create a map.  The logsize parameter specifies the expected number of
 // entries in the map (e.g. logsize = 10 for 1024).  There is a fairly small
 // but not completely negligible chance of failure to build the map (this is
 // true for any hash-based implementation which limits its worst-case
 // behavior.)  The failure rate depends on the initial logsize value:
 // the bigger the table, the smaller the chance that it breaks.  Therefore,
-// logsize should not be too small, and had better be a realistic lower bound.
-struct fpmap *fpmap_new(int logsize);
-void fpmap_free(struct fpmap *map);
+// logsize should not be too small, and had better be a realistic minimum.
+struct fp47map *fp47map_new(int logsize);
+void fp47map_free(struct fp47map *map);
 
 // Since the buckets are fixed-size, the map guarantees O(1) worst-case lookup.
-// Use FPMAP_MAXFIND to specify the array size for fpmap_find().
-#define FPMAP_MAXFIND 10
+// Use FP47MAP_MAXFIND to specify the array size for fp47map_find().
+#define FP47MAP_MAXFIND 10
 #ifdef __cplusplus
-#define FPMAP_pMAXFIND 10 // for use in function prototypes
+#define FP47MAP_pMAXFIND 10 // for use in function prototypes
 #else
-#define FPMAP_pMAXFIND static 10
+#define FP47MAP_pMAXFIND static 10
 #endif
 
 // i386 convention: on Windows, stick to fastcall, for compatibility with msvc.
 #if (defined(_WIN32) || defined(__CYGWIN__)) && \
     (defined(_M_IX86) || defined(__i386__))
-#define FPMAP_MSFASTCALL 1
+#define FP47MAP_MSFASTCALL 1
 #if defined(__GNUC__)
-#define FPMAP_FASTCALL __attribute__((fastcall))
+#define FP47MAP_FASTCALL __attribute__((fastcall))
 #else
-#define FPMAP_FASTCALL __fastcall
+#define FP47MAP_FASTCALL __fastcall
 #endif
 #else // otherwise, use regparm(3).
-#define FPMAP_MSFASTCALL 0
+#define FP47MAP_MSFASTCALL 0
 #if defined(__i386__)
-#define FPMAP_FASTCALL __attribute__((regparm(3)))
+#define FP47MAP_FASTCALL __attribute__((regparm(3)))
 #else
-#define FPMAP_FASTCALL
+#define FP47MAP_FASTCALL
 #endif
 #endif
 
 // fastcall has trouble passing uint64_t in registers.
-#if FPMAP_MSFASTCALL
-#define FPMAP_pFP64 uint32_t lo, uint32_t hi
-#define FPMAP_aFP64(fp) fp, fp >> 32
+#if FP47MAP_MSFASTCALL
+#define FP47MAP_pFP64 uint32_t lo, uint32_t hi
+#define FP47MAP_aFP64(fp) fp, fp >> 32
 #else
-#define FPMAP_pFP64 uint64_t fp
-#define FPMAP_aFP64(fp) fp
-#endif
-
-// Check if CPU registers are 64-bit.
-#if (SIZE_MAX > UINT32_MAX) || defined(__x86_64__)
-#define FPMAP_REG64 1
-#else
-#define FPMAP_REG64 0
-#endif
-
-// To reduce the failure rate, one or two bucket entries can be stashed.
-struct fpmap_stash {
-    struct fpmap_bent be[2];
-    // Since bucket entries are looked up by index+fptag, we also need to
-    // remember the index (there are actually two symmetrical indices and
-    // we store the smaller one).  Furthermore, on 64-bit platforms we can
-    // check index+fptag as a single var.
-#if FPMAP_REG64
-    uint64_t lo[2];
-#else
-    uint32_t lo[2];
-#endif
-};
-
-#if FPMAP_BENT_SIZE * 8 - FPMAP_FPTAG_BITS == 32
-#define FPMAP_FIND32 1
-#else
-#define FPMAP_FIND32 0
+#define FP47MAP_pFP64 uint64_t fp
+#define FP47MAP_aFP64(fp) fp
 #endif
 
 // Expose the structure, to inline vfunc calls.
-struct fpmap {
+struct fp47map {
+    // To reduce the failure rate, one or two bucket entries can be stashed.
+    // There are some details which we do not disclose in this header file.
     // This guy goes first and gets the best alignment (the same as buckets).
-    struct fpmap_stash stash;
+    unsigned char stash[24];
     // Virtual functions, depend on the bucket size, switched on resize.
     // Pass fp arg first, eax:edx may hold hash() return value.
-    size_t (FPMAP_FASTCALL *find)(FPMAP_pFP64, const struct fpmap *set,
-	    struct fpmap_bent *match[FPMAP_pMAXFIND]);
-#if FPMAP_FIND32
-    size_t (FPMAP_FASTCALL *find32)(FPMAP_pFP64, const struct fpmap *set,
-	    uint32_t match[FPMAP_pMAXFIND]);
-#endif
-    struct fpmap_bent *(FPMAP_FASTCALL *insert)(FPMAP_pFP64, struct fpmap *map);
-    void (FPMAP_FASTCALL *prefetch)(FPMAP_pFP64, const struct fpmap *map);
+    unsigned (FP47MAP_FASTCALL *find)(FP47MAP_pFP64, const struct fp47map *map,
+	    uint32_t mpos[FP47MAP_pMAXFIND]);
+    int (FP47MAP_FASTCALL *insert)(FP47MAP_pFP64, struct fp47map *map, uint32_t pos);
+    void (FP47MAP_FASTCALL *prefetch)(FP47MAP_pFP64, const struct fp47map *map);
     // The buckets (malloc'd); each bucket has bsize entries.
-    // Two-dimensional structure is emulated with pointer arithmetic.
-    struct fpmap_bent *bb;
+    void *bb;
     // The total number of entries added to buckets,
     // not including the stashed entries.
     size_t cnt;
@@ -187,40 +127,31 @@ struct fpmap {
     uint32_t mask0, mask1;
 };
 
-// Obtain the set of entries matching the fingerprint.
-// Returns the number of matches found (up to FPMAP_MAXFIND, typically 0 or 1).
-static inline size_t fpmap_find(const struct fpmap *map, uint64_t fp,
-	struct fpmap_bent *match[FPMAP_pMAXFIND])
+// Obtain the set of positions matching a fingerprint.
+// Returns the number of matches found (up to FP47MAP_MAXFIND, typically 0 or 1).
+static inline unsigned fp47map_find(const struct fp47map *map, uint64_t fp,
+	uint32_t mpos[FP47MAP_pMAXFIND])
 {
-    return map->find(FPMAP_aFP64(fp), map, match);
+    return map->find(FP47MAP_aFP64(fp), map, mpos);
 }
 
-#if FPMAP_FIND32
-static inline size_t fpmap_find32(const struct fpmap *map, uint64_t fp,
-	uint32_t match[FPMAP_pMAXFIND])
+// Insert a new entry, that is, a new position associated with a fingerprint.
+static inline int fp47map_insert(struct fp47map *map, uint64_t fp, uint32_t pos)
 {
-    return map->find32(FPMAP_aFP64(fp), map, match);
-}
-#endif
-
-// Request a new entry associated with the fingerprint.
-static inline struct fpmap_bent *fpmap_insert(struct fpmap *map, uint64_t fp)
-{
-    return map->insert(FPMAP_aFP64(fp), map);
+    return map->insert(FP47MAP_aFP64(fp), map, pos);
 }
 
-// Prefetch the buckets related to the fingerprint.
-static inline void fpmap_prefetch(const struct fpmap *map, uint64_t fp)
+// Prefetch the buckets related to a fingerprint.
+static inline void fp47map_prefetch(const struct fp47map *map, uint64_t fp)
 {
-    map->prefetch(FPMAP_aFP64(fp), map);
+    map->prefetch(FP47MAP_aFP64(fp), map);
 }
 
-// Iterate bucket entries.  iter must be initially set to zero;
-// it stores the position of "where we left off".  On each call, a pointer
-// to an entry is returned and the position is advanced.  After all the
-// entries are traversed, returns NULL and resets iter to zero.
-struct fpmap_bent *FPMAP_FASTCALL fpmap_next(const struct fpmap *map,
-					     size_t *iter);
+// Iterate the bucket entries (in no particular order).  iter must be
+// initially set to zero; it picks up where we left off.  On each call,
+// the pointer to a next position is returned as the iterator gets advanced.
+// After all the entries are traversed, returns NULL and resets iter to zero.
+uint32_t *FP47MAP_FASTCALL fp47map_next(const struct fp47map *map, size_t *iter);
 
 #ifdef __GNUC__
 #pragma GCC visibility pop
